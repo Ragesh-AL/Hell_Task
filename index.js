2cefa48798136d1ce0f51bc98ed3620f48159b29
Ragesh AL <161115673+Ragesh-AL@users.noreply.github.com>
2024-05-05 03:20:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+// let array=[1,3,4,3,5,3,2]
+// array.forEach((e)=>{
+//     console.log(e)
+// })
+
+setTimeout(()=>{
+    let print=document.getElementById("display")
+    print.innerText="10"
+    setTimeout(()=>{
+        let print=document.getElementById("display")
+        print.innerText="9"
+        setTimeout(()=>{
+            let print=document.getElementById("display")
+            print.innerText="8"
+            setTimeout(()=>{
+                let print=document.getElementById("display")
+                print.innerText="7"
+                setTimeout(()=>{
+                    let print=document.getElementById("display")
+                    print.innerText="6"
+                    setTimeout(()=>{
+                        let print=document.getElementById("display")
+                        print.innerText="5"
+                        setTimeout(()=>{
+                            let print=document.getElementById("display")
+                            print.innerText="4"
+                            setTimeout(()=>{
+                                let print=document.getElementById("display")
+                                print.innerText="3"
+                                setTimeout(()=>{
+                                    let print=document.getElementById("display")
+                                    print.innerText="2"
+                                    setTimeout(()=>{
+                                        let print=document.getElementById("display")
+                                        print.innerText="1"
+                                        setTimeout(()=>{
+                                            let print=document.getElementById("display")
+                                            print.innerText="Happy independence day"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
